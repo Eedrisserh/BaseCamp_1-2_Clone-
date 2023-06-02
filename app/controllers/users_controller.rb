925f0fe707821896b17d81549bf1bba5cb5bda03
Eedrisserh <edrissebraheem@gmail.com>
2023-06-02 22:18:18 +0100

    Everything working now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,71 +1,83 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+    before_action :authenticate_user!
 
-  def index
-    if current_user.role == 1
-      @users = User.all
-    else
-      redirect_to user_path(current_user)
+    def index
+        if current_user.role == 1
+            @users = User.all
+        else
+            redirect_to user_path(current_user)
+        end
     end
-  end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to users_path
-    else
-      render :new
+    def new
+        @user = User.new
     end
-  end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to users_path
-    else
-      render :edit
+    def create
+        @user = User.new(permitted_params)
+        if @user.save
+            redirect_to users_path
+        else
+            render :new
+        end
     end
-  end
 
-  def update_role
-    @user = User.find(params[:id])
-    if @user.update(role_params)
-      redirect_to users_path, notice: 'Role updated successfully'
-    else
-      redirect_to users_path, alert: 'Failed to update role'
+    def edit
+        @user = User.find(params[:id])
     end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to users_path
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation)
-  end
-
-  def role_params
-    params.require(:user).permit(:role)
-  end
-
-  def password_confirmation_matches
-    if params[:user][:password].present? && params[:user][:password_confirmation].present?
-      if params[:user][:password] != params[:user][:password_confirmation]
-        # add an error to the @user object
-        @user.errors.add(:password_confirmation, "must match password")
-      end
+    
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+            if @user.errors.any?
+                render :edit
+            else
+                redirect_to @user, notice: 'User updated successfully'
+            end
     end
-  end
+
+    def update_role
+        @user = User.find(params[:id])
+        @user.update(role_params)
+        notice = 'Role updated successfully'
+    end
+
+    def show
+        if params[:id] == 'sign_out'
+            sign_out
+            redirect_to root_path
+        else
+          @user = User.find(params[:id])
+        end
+    end
+      
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_path
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation)
+    end
+
+    def permitted_params
+        params.require(:user_registration).permit(:email, :password, :password_confirmation, :admin)
+    end
+
+    def role_params
+        params.require(:user).permit(:role)
+    end
+
+    def password_confirmation_matches
+        if params[:user][:password].present? && params[:user][:password_confirmation].present?
+          if params[:user][:password] != params[:user][:password_confirmation]
+            # add an error to the @user object
+            @user.errors.add(:password_confirmation, "must match password")
+          end
+        end
+    end
 end
